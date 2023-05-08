b0b75767f102a125883a8562cc9d1e9029cd05c2
devyank <devyanknagpal2002@gmail.com>
2023-05-08 16:45:02 +0530

    Revert "put req"
    This reverts commit be3b14104d1c88fed579eb809279613e96174160.
    
    "changes"

--- pages/profile/edit/profile.tsx
+++ pages/profile/edit/profile.tsx
@@ -76,6 +76,7 @@ const top100Films = [
     { title: 'Redux' },
     { title: 'Ruby' },
     { title: 'Rust' },
+    { title: 'Rust' },
     { title: 'Scala' },
     { title: 'SQLite' },
     { title: 'Swift' },
@@ -91,18 +92,18 @@ const emailRef=useRef<HTMLInputElement>(null);
 const portRef=useRef<HTMLInputElement>(null);
 const expRef=useRef<HTMLInputElement>(null);
 const aboutRef=useRef<HTMLInputElement>(null);
-const sRef=useRef<HTMLInputElement>();
+const sRef=useRef<HTMLInputElement>(null);
 const [skillRef,setskillRef]=useState<string[]>([]);
 
 const handleprofile=(e:React.FormEvent)=>{
 e.preventDefault();
 const pro={
- name:nameRef.current?.value,
-  email:emailRef.current?.value,
-  portfoilo:portRef.current?.value,
-  experience:expRef.current?.value,
-  about:aboutRef.current?.value,
-  skills:skillRef
+  nameRef,
+  emailRef,
+  portRef,
+  expRef,
+  aboutRef,
+  sRef
 
 }
 console.log(pro)
@@ -235,7 +236,7 @@ console.log(pro)
         filterSelectedOptions
         renderInput={(params) => (
           <TextField
-        
+          inputRef={sRef}
             {...params}
             label="Add skills"
             placeholder="Skills"
