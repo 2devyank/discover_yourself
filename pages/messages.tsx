16c1430613bfa03101fe7ee0aa9f7d4dc99484e3
devyank <devyanknagpal2002@gmail.com>
2023-06-07 23:30:21 +0530

    adding message to db and auto scrolling of message div functionality completed

--- pages/messages.tsx
+++ pages/messages.tsx
@@ -1,20 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "../styles/message.module.css"
 import { useConverseQuery } from '@/features/Converse'
 import Meslist from './components/Meslist'
-import { useMessageQuery } from '@/features/Message'
+import { useMessageQuery,usePostmessageMutation } from '@/features/Message'
 import Messenger from './components/Messenger'
 
 
 
 export default function messages() {
   const [currentchat,setcurrentchat]=useState<number>();
+  // const [newmessage,setnewmessage]=useState<string>("");
+  const newmessage=useRef<HTMLTextAreaElement>(null);
+  const viewref=useRef<HTMLDivElement>(null);
  
   const id = typeof window !== 'undefined' ? localStorage.getItem('userid') : null
   const {data,isLoading,isSuccess,error}=useConverseQuery(id as unknown as void);
 
 const {data:mdata,isSuccess:misSuccess}=useMessageQuery(currentchat as unknown as void);
     console.log(mdata);
+
+const [Addmes,result]=usePostmessageMutation();
+
+const submitmessage=async(e: { preventDefault: () => void; })=>{
+e.preventDefault();
+
+const mes={
+con_id:currentchat,
+text:newmessage.current?.value,
+sender:id
+}
+await Addmes(mes);
+
+}
+useEffect(()=>{
+viewref.current?.scrollIntoView({behavior:"smooth"})
+},[mdata])
 
   return (
     <div className={styles.allmes}>
@@ -32,13 +52,28 @@ const {data:mdata,isSuccess:misSuccess}=useMessageQuery(currentchat as unknown a
             }
             {
               misSuccess &&
+              <div className={styles.coverright}>
+
         <div className={styles.rightmes}>
+
           {
             mdata.map((mdata)=>(
+              <div ref={viewref} className={styles.innerright}>
               <Messenger mesdata={mdata} own={mdata.sender.toString()===id }/>
-            ))
-          }
+          </div>
+              ))
+            }
         </div>
+        <div className={styles.sendmes}>
+<textarea 
+name="" id="" 
+cols={25} 
+rows={5} 
+ref={newmessage}
+></textarea>
+<button onClick={submitmessage}>Send</button>
+        </div>
+            </div>
         }
         
     </div>
